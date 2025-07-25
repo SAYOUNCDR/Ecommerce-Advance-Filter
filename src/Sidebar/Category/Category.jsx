0ace329f31363aff73025923ecd764388b4adb0b
SAYOUNCDR <pheonixgamer12345@gmail.com>
2025-07-25 14:54:35 +0530

    Input Comp prop issue fixed

--- src/Sidebar/Category/Category.jsx
+++ src/Sidebar/Category/Category.jsx
@@ -5,11 +5,35 @@ const Category = (handleChange) => {
     <div>
       <h2 className="sidebar-title">Category</h2>
       <div>
-        <Input />
-        <Input />
-        <Input />
-        <Input />
-        <Input />
+        <label className="sidebar-label-container">
+          <input type="radio" onChange={handleChange} value="" name="test" />
+          <span className="checkmark"></span>All
+        </label>
+
+        <Input
+          handleChange={handleChange}
+          value="sneakers"
+          title="Sneakers"
+          name="test"
+        />
+        <Input
+          handleChange={handleChange}
+          value="flats"
+          title="Flats"
+          name="test"
+        />
+        <Input
+          handleChange={handleChange}
+          value="sandals"
+          title="Sandals"
+          name="test"
+        />
+        <Input
+          handleChange={handleChange}
+          value="hells"
+          title="Hells"
+          name="test"
+        />
       </div>
     </div>
   );
